6ff2379f9af7909f04a10297367bb69c97831c31
cole <mcg42387@gmail.com>
2013-06-30 22:57:09 -0400

    added fox's latest news spider

---
+++ foxNews/latest.js
@@ -0,0 +1,29 @@
+var $ = require('cheerio'),
+  request = require('request')
+
+function gotHTML(err, resp, html) {
+
+  if (err) return console.error(err)
+
+  var parsedHTML = $.load(html),
+    alreadyAdded;
+
+  parsedHTML('.latest-news > ul > li > a ').map(function(i, latest) {
+
+    var href = $(latest).attr('href')
+
+    // do not show the links twice, cnn adds video imgs with the same
+    // href as the text beside it
+    if(alreadyAdded !== href) {
+
+      alreadyAdded = href
+
+      console.log(href);
+
+    }
+
+  }) // end parsedHTML()
+
+} // end gotHTML()
+
+request("http://www.foxnews.com/", gotHTML)
