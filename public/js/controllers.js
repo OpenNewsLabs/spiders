9972c639ca2e10524a0b746883b33df6ffb9107c
Cole Gillespie <mcg42387@gmail.com>
2014-12-31 10:36:36 -0500

    added cnn and fox to lists

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,6 +6,15 @@ var openNews = angular.module('openNews', []);
 
 openNews.controller('newsListCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('/bbc').success(function(data) {
-    $scope.stories = data;
+    $scope.bbc = data;
   });
+
+  $http.get('/fox').success(function(data) {
+    $scope.fox = data;
+  });
+
+  $http.get('/cnn').success(function(data) {
+    $scope.cnn = data;
+  });
+
 }]);
